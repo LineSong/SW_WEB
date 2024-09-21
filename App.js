ab282b47717c1cd3ada5d441d4fc8d68f6469b6f
SAILGO <121847777+SA-ILGO@users.noreply.github.com>
2024-09-21 11:03:20 +0900

    Revert "[feature] 엔티티 구조 정의 및 view.html에서 테스트"

--- App.js
+++ App.js
@@ -1,33 +1,30 @@
 const express = require('express');
 const path = require('path');
-const mysql = require('mysql2/promise');
-const dbConfig = require('./config/database');
+const cors = require('cors');
+const usersRouter = require('./usersRouter');
 
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'view.html'));
-});
+app.use(express.static(path.join(__dirname, '/Build')));
+app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/api/data', async (req, res) => {
-    const connection = await mysql.createConnection(dbConfig);
-    try {
-        const [students] = await connection.execute('SELECT * FROM student');
-        const [lines] = await connection.execute('SELECT * FROM line');
-        res.json({ students, lines });
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        res.status(500).json({ error: 'Error fetching data' });
-    } finally {
-        await connection.end();
-    }
-});
+app.use(`/users`, usersRouter);
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+app.get('/webgl', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Build', 'index.html'));
+  });
+
+app.get('/manage', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'manage.html'));
+  });
+
+app.listen(port, ()=>
+{
+   console.log(`SERVER 실행됨 ${port}`); 
 });
-// node app.js (터미널에서 이 코드로 실행)
+// node app.js
 //http://127.0.0.1:3000
